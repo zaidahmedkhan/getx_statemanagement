d3a3cefa34e9e2f724b5ab94887279b06268e326
John Doe <zaidkhan200415@gmail.com>
2024-07-08 21:10:26 +0500

    getx change theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -26,32 +26,62 @@ class _HomeScreenState extends State<HomeScreen> {
               subtitle: const Text("Dualog alert with getx"),
               onTap: () {
                 Get.defaultDialog(
-                    title: "Delete chat",
-                    titlePadding: const EdgeInsets.only(top: 20),
-                    contentPadding: const EdgeInsets.all(20),
-                    // middleText: "Are you sure you want to delete this chat?",
-                    textConfirm: "yes",
-                    textCancel: "no",
-                    content: Column(
-                      children: const [
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                        Text("cancel"),
-                      ],
-                    ));
+                  title: "Delete chat",
+                  titlePadding: const EdgeInsets.only(top: 20),
+                  contentPadding: const EdgeInsets.all(20),
+                  middleText: "Are you sure you want to delete this chat?",
+                  textConfirm: "yes",
+                  textCancel: "no",
+                  // content: Column(
+                  //   children: const [
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //     Text("cancel"),
+                  //   ],
+                  //  )
+                );
               },
             ),
-          )
+          ),
+          Card(
+            child: ListTile(
+              title: const Text("Getx Bottom sheet "),
+              subtitle: const Text("Bottom sheet with getx"),
+              onTap: () {
+                Get.bottomSheet(Container(
+                  color: Colors.red,
+                  child: Column(
+                    children: [
+                      ListTile(
+                        leading: Icon(Icons.light_mode),
+                        title: Text("Light theme"),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.light());
+                        },
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.dark_mode),
+                        title: Text("dark theme"),
+                        onTap: () {
+                          Get.changeTheme(ThemeData.dark());
+                        },
+                      ),
+                    ],
+                  ),
+                ));
+              },
+            ),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      // floatingActionButton: FloatingActionButton(onPressed: () {}),
     );
   }
 }
